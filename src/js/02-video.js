0aa872f82cf366ac7bd07752e8a7d84fbe504afd
ivaniv_maks <ivanivmaks0@gmail.com>
2023-05-25 11:43:01 +0300

    updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player(document.querySelector('iframe'));
+player.on(
+  'timeupdate',
+  throttle(event => {
+    const time = event.seconds;
+    localStorage.setItem('videoplayer-current-time', time);
+  }, 1000)
+);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,5 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const formEmail = form.querySelector('input[type="email"]');
+const formMessage = form.querySelector('textarea[name="message"]');
