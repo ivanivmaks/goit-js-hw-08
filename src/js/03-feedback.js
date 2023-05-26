da60bbb2a0c874788507cf75a821b61a81a53a5e
ivaniv_maks <ivanivmaks0@gmail.com>
2023-05-26 13:00:37 +0300

    updated

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,3 +3,40 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const formEmail = form.querySelector('input[type="email"]');
 const formMessage = form.querySelector('textarea[name="message"]');
+
+function savedFormData() {
+  const formData = {
+    email: formEmail.value,
+    message: formMessage.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+form.addEventListener('input', throttle(savedFormData, 500));
+
+function loadPage() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  const parseData = JSON.parse(savedData);
+  if (savedData) {
+    formEmail.value = parseData.email;
+    formMessage.value = parseData.message;
+  } else {
+    formEmail.value = '';
+    formMessage.value = '';
+  }
+}
+
+window.addEventListener('load', loadPage);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = {
+    email: formEmail.value,
+    message: formMessage.value,
+  };
+  localStorage.removeItem('feedback-form-state');
+  formEmail.value = '';
+  formMessage.value = '';
+  console.log(formData);
+}
+form.addEventListener('submit', handleSubmit);
