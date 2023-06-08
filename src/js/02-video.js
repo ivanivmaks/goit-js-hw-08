e0bd42d505aa6f23944110028f2f8ddc693e06a3
ivaniv_maks <ivanivmaks0@gmail.com>
2023-06-08 20:38:31 +0300

    updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,14 +2,9 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const player = new Player(document.querySelector('iframe'));
-player.on(
-  'timeupdate',
-  throttle(event => {
-    localStorage.setItem('videoplayer-current-time', event.seconds);
-  }, 1000)
-);
-
-const savedTime = localStorage.getItem('videoplayer-current-time');
-if (savedTime) {
-  player.setCurrentTime(savedTime);
+function onTimeUpdate(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
 }
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
